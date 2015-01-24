4a829c5cc003e5b4d460653d420f471971d04067
dburkha1 <dburkha1@my.westga.edu>
2015-01-24 17:46:14 -0500

    Created all web forms and classes needed for the project

---
+++ App_Code/CustomerList.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for CustomerList
+/// </summary>
+public class CustomerList
+{
+	public CustomerList()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+}
---
+++ App_Code/Description.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Description
+/// </summary>
+public class Description
+{
+	public Description()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+}
